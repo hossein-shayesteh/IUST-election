36ed3abb134f03bc10713e3f8cc5543510684f11
Hossein Shayesteh <hosseinxshayesteh@gmail.com>
2025-05-19 09:45:05 +0330

    Initial Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,12 @@
+import React from "react";
+
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Vazirmatn } from "next/font/google";
+
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const vazirmatn = Vazirmatn({
+  variable: "--font-vazirmatn-sans",
   subsets: ["latin"],
 });
 
@@ -24,11 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${vazirmatn.className} antialiased`}>{children}</body>
     </html>
   );
 }
